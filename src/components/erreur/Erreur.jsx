8eb8efd54683d0055ebd547cc2bd3de23d3cc152
ghilasameur@yahoo.com <ghilasameur@yahoo.com>
2021-02-13 14:55:04 +0100

    gestio

--- src/components/erreur/Erreur.jsx
+++ src/components/erreur/Erreur.jsx
@@ -1,15 +1,28 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Navbar from "../accueil/Navbar";
 import Footer from "../accueil/Footer";
+import { Link } from "react-router-dom";
 
 function Erreur() {
   return (
     <div>
       <Navbar />
+      <div className="container-fluid bg-dark text-white div-form ">
+        <h2 className="p-2">Popular Titles</h2>
+
+        <div className="div-form div-form1"></div>
+      </div>
 
       <div className="container" style={{ height: 340 }}>
         {" "}
-        <h3 className="text-center p-5">Oups !! cette page n'esxiste pas !</h3>
+        <h3 className="text-center p-3"> Oops</h3>
+        <h4 className="text-center">
+          {" "}
+          The page you were looking for doesn't exist
+        </h4>
+        <Link to="/">
+          <p className="text-center p-5 ">Take me back to the home page </p>
+        </Link>
       </div>
       <Footer />
     </div>
